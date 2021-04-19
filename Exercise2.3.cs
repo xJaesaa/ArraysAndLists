27c73ed29f649197225bbc7fb55123c0487583d9
xJaesaa <103669129@student.swin.edu.au>
2021-04-19 22:18:10 +1000

    gamer time

--- Exercise2.3.cs
+++ Exercise2.3.cs
@@ -9,7 +9,21 @@ namespace aandltask_2_3
     {
         static void Main_2_3(string[] args)
         {
-            
+            List<string> words = new List<string>{};
+            string insert = "gamer";
+
+            while(insert != "stop"){
+                Console.WriteLine("Enter word to add");
+                insert = Console.ReadLine();
+                if(insert == "stop"){
+                    break;
+                }
+                words.Add(insert);
+            }
+
+            for(int i = words.Count()-1; i >= 0; i--){
+                Console.Write(words[i] + " ");
+            }
         }
     }
 }
